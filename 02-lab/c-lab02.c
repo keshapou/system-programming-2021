489509ab47ccde2b1acbc8c3621d5eada61726f1
kesha <swarovski991@gmail.com>
2021-03-23 03:20:56 -0400

    changed files

--- 02-lab/c-lab02.c
+++ 02-lab/c-lab02.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
     const size_t SIZE = 10;
-    __uint8_t arr[] = {127, 255, 2, 3, 4, 5, 6, 7, 8, 9};
-    __uint8_t bitmask = 34; // same as 00100010
+    uint8_t arr[] = {127, 255, 2, 3, 4, 5, 6, 7, 8, 9};
+    uint8_t bitmask = 34; // same as 00100010
 
-    __uint16_t sum = 0;
+    uint16_t sum = 0;
     for (int i = 0; i < SIZE; ++i)
     {
-        __uint8_t res = arr[i] & bitmask;
+        uint8_t res = arr[i] & bitmask;
         if (res == bitmask)
         {
             sum += arr[i];
